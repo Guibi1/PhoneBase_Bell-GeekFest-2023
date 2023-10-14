72943e7e4a977411f8fa6636005587cfe610fe70
Laurent Stéphenne <laurent@guibi.ca>
2023-10-14 14:53:59 -0400

    Created base app shell...

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,21 @@
-<script>import "../app.postcss";</script>
+<script lang="ts">
+    import { AppShell, AppBar } from "@skeletonlabs/skeleton";
+    import logoTitle from "$assets/PhoneBase-logo.png";
 
-<slot></slot>
+    import "../app.postcss";
+</script>
+
+<AppShell>
+    <svelte:fragment slot="header">
+        <AppBar background="bg-surface-500">
+            <svelte:fragment slot="lead">
+                <img class="h-20" src={logoTitle} alt="logo" />
+            </svelte:fragment>
+
+            <h1 class="h1">PhoneBase</h1>
+            <h2 class="h4">Quantum-Secured Password Manager</h2>
+        </AppBar>
+    </svelte:fragment>
+
+    <slot />
+</AppShell>
