86d7cc96f5deb919d8b859eef017cf15f12c478f
Laurent Stéphenne <laurent@guibi.ca>
2023-10-14 01:08:29 -0400

    Added vercel adapter

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import type { Config } from "@sveltejs/kit";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
