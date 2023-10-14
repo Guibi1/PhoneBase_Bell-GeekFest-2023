dd1314203ae1908946c8f14bc0795d33443cabf9
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-10-14 03:15:52 -0400

    Created database

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,8 +1,9 @@
 import { DATABASE_HOST, DATABASE_PASSWORD, DATABASE_USERNAME } from "$env/static/private";
 import * as schemas from "$lib/schemas";
 import { connect } from "@planetscale/database";
-import { eq } from "drizzle-orm";
+import { and, eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { generatePrivateKey, generateUserId } from "./generate";
 
 const connection = connect({
     host: DATABASE_HOST,
@@ -22,5 +23,33 @@ export async function findUser(phone: string) {
 }
 
 export async function addPassword(user: schemas.User, website: string, password: string) {
-    await db.insert(schemas.passwords).values({ id: 3, userId: user.id, website, password });
+    await db.insert(schemas.passwords).values({ userId: user.id, website, password });
+}
+
+export async function removePassword(user: schemas.User, website: string) {
+    await db
+        .delete(schemas.passwords)
+        .where(and(eq(schemas.passwords.website, website), eq(schemas.passwords.userId, user.id)));
+}
+export async function modifyPassword(user: schemas.User, website: string, password: string) {
+    await db
+        .update(schemas.passwords)
+        .set({ password })
+        .where(and(eq(schemas.passwords.website, website), eq(schemas.passwords.userId, user.id)));
+}
+export async function getPassword(user: schemas.User, website: string) {
+    await db
+        .select({ password: schemas.passwords.password })
+        .from(schemas.passwords)
+        .where(and(eq(schemas.passwords.website, website), eq(schemas.passwords.userId, user.id)));
+}
+export async function createUser(phoneNumber: string) {
+    const id = generateUserId();
+    const privatekey = generatePrivateKey();
+
+    // const res = await fetch("/api/encrypt/private")
+    const publicKey = "[12,234]";
+
+    await db.insert(schemas.users).values({ id, publicKey });
+    await db.insert(schemas.phones).values({ userId: id, number: phoneNumber });
 }
